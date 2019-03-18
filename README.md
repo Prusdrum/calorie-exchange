f84aa8f03c7344396ebacf076670031cbe4c86e9
prusdrum <mateusz.prusaczyk@gmail.com>
2019-03-18 10:15:07 +0100

    fruits are ready

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # calorie-exchange
 App to calculate calorie equivalent of given food
+
+# [Live demo](https://prusdrum.github.io/calorie-exchange)
--- src/calculator-view-model.js
+++ src/calculator-view-model.js
@@ -1,16 +1,41 @@
 const ko = require('knockout')
 const _ = require('lodash')
+const products = require('./data.json')
 
 class CalculatorViewModel {
     constructor() {
-        this.products = ko.observableArray([
-            { 
-                name: 'Banan', selected: ko.observable(false), id: ko.observable(_.uniqueId())
-            },
-            { 
-                name: 'Jabłko', selected: ko.observable(false), id: ko.observable(_.uniqueId())
-            },
-        ])
+        this.inWeight = ko.observable(100);
+        this.selectedProduct = ko.observable(null);
+        this.products = ko.observableArray(products.map(data => {
+            return {
+                id: ko.observable(_.uniqueId('product-')),
+                name: data.name,
+                value: data.value,
+                selected: ko.observable(false)
+            }
+        }))
+
+        this.results = ko.computed(() => {
+            if (this.selectedProduct() === null) {
+                return [];
+            }
+
+            return this.products().map((product) => {
+                const newVal = (product.value * this.inWeight()) / this.selectedProduct().value;
+                return {
+                    name: product.name,
+                    value: Math.round((newVal * 100) / 100)
+                }
+            })
+        });
+    }
+
+    onChange(product) {
+        this.selectedProduct(product);
+    }
+
+    isSelected(product) {
+        return product === this.selectedProduct()
     }
 }
 
