dd09fd15e6ec5cf773a6e5feb61dca2cf7424384
prusdrum <mateusz.prusaczyk@gmail.com>
2019-03-18 11:00:23 +0100

    vegetables

--- src/calculator-view-model.js
+++ src/calculator-view-model.js
@@ -1,19 +1,40 @@
 const ko = require('knockout')
 const _ = require('lodash')
-const products = require('./data.json')
+const fruits = require('./fruits.json')
+const vegetables = require('./vegetables.json')
+
+const FRUITS = 'fruits';
+const VEGETABLES = 'vegetables'
 
 class CalculatorViewModel {
     constructor() {
+        this.selectedGroup = ko.observable(FRUITS)
         this.inWeight = ko.observable(100);
         this.selectedProduct = ko.observable(null);
-        this.products = ko.observableArray(products.map(data => {
-            return {
-                id: ko.observable(_.uniqueId('product-')),
-                name: data.name,
-                value: data.value,
-                selected: ko.observable(false)
+        this.products = ko.computed(() => {
+            if (this.fruitsSelected()) {
+                return _.sortBy(fruits.map(data => {
+                    return {
+                        id: ko.observable(_.uniqueId('product-')),
+                        name: data.name,
+                        value: data.value,
+                        selected: ko.observable(false)
+                    }
+                }), (i) => i.name)
+            } else {
+                return _.sortBy(vegetables.map(data => {
+                    return {
+                        id: ko.observable(_.uniqueId('product-')),
+                        name: data.name,
+                        value: data.value,
+                        selected: ko.observable(false)
+                    }
+                }), (i) => i.name)
             }
-        }))
+        })
+        this.selectedGroup.subscribe(() => {
+            this.selectedProduct(null);
+        });
 
         this.results = ko.computed(() => {
             if (this.selectedProduct() === null) {
@@ -24,10 +45,28 @@ class CalculatorViewModel {
                 const newVal = (product.value * this.inWeight()) / this.selectedProduct().value;
                 return {
                     name: product.name,
-                    value: Math.round((newVal * 100) / 100)
+                    value: newVal.toFixed(1)
                 }
             })
         });
+
+        this.reminder = ko.computed(() => {
+            if (!this.selectedProduct()) {
+                return '';
+            }
+            const product = this.selectedProduct();
+            return `Wybrane: ${product.name} ${this.inWeight()} [gram] - ${this.getCalories().toFixed(1)} kcal`
+        });
+    }
+
+    getCalories() {
+        const FRUITS_CALORIES_FOR_BASE = 47;
+        const VEGETABLES_CALORIES_FOR_BASE = 29;
+        if (this.fruitsSelected()) {
+            return this.inWeight() * FRUITS_CALORIES_FOR_BASE / this.selectedProduct().value
+        } else {
+            return this.inWeight() * VEGETABLES_CALORIES_FOR_BASE / this.selectedProduct().value
+        }
     }
 
     onChange(product) {
@@ -36,6 +75,22 @@ class CalculatorViewModel {
 
     isSelected(product) {
         return product === this.selectedProduct()
+    }
+
+    turnFruits() {
+        this.selectedGroup(FRUITS)
+    }
+
+    turnVegetables() {
+        this.selectedGroup(VEGETABLES)
+    }
+
+    fruitsSelected() {
+        return this.selectedGroup() === FRUITS;
+    }
+
+    vegetablesSelected() {
+        return this.selectedGroup() === VEGETABLES;
     }
 }
 
